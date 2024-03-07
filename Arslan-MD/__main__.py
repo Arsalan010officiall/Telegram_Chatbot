971063ead0abdd34678ca37712c8f36045d5b517
A-R-S-L-A-N_M-D <134489993+Arsalan010officiall@users.noreply.github.com>
2024-03-07 13:17:41 +0500

    Add files via upload

---
+++ Arslan-MD/__main__.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2021 Itz-fork
+
+from pyrogram import idle
+from . import yuiai
+
+if __name__ == "__main__" :
+    yuiai.start()
+    print("Yui is live! Join @NexaBotsUpdates")
+    idle()
